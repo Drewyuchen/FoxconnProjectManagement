2c943fcb22d92f18ce162dc49550b9981ff5fab2
= <=>
2020-05-07 10:04:17 +0800

    first commit

--- src/components/emp_job_hour.vue
+++ src/components/emp_job_hour.vue
@@ -10,18 +10,27 @@
     <el-divider></el-divider>
     <h1 style="text-align: center">富士康項目管理系統</h1>
     <p style="text-align: center">請在此選擇您的項目時程（項目開始與結束時間）</p>
-    <span class="demonstration"></span>
-    <el-date-picker
-      v-model="value1"
-      type="datetimerange"
-      start-placeholder="开始日期時間"
-      end-placeholder="结束日期時間"
-      :default-time="['12:00:00']">
-    </el-date-picker>
+    <div class="blockStartTime">
+      <span class="demonstration">開始時間</span>
+      <el-date-picker
+        v-model="value1"
+        type="datetime"
+        placeholder="选择日期时间">
+      </el-date-picker>
+    </div>
+    <div class="blockEndTime">
+      <span class="demonstration">結束時間</span>
+      <el-date-picker
+        v-model="value2"
+        type="datetime"
+        placeholder="选择日期时间">
+      </el-date-picker>
+    </div>
     <div class="confirmButton">
-      <el-link type="primary" href="emp_job_complete">點擊確認完成</el-link>
+      <el-link type="primary" href="emp_job_complete">點擊已確認完成</el-link>
     </div>
     <el-divider></el-divider>
+
     <div class="Footer">
       This is Footer
     </div>
@@ -53,8 +62,10 @@
   .el-date-picker{ text-align:center ;opacity: 80% ;margin-left: 10px; margin-right: 10px;margin-top: 10px}
   .el-date-editor{width: 100%}
   .el-page-header{color:dodgerblue; font-size:40px}
-  .el-link{font-size: larger;margin-top: 10px;text-align: center}
+  .el-link{font-size: larger;margin-top: 10px;text-align: center;vertical-align: center}
   .Footer{text-align: center;vertical-align: center;horiz-align: center}
   .confirmButton{text-align: center}
+  .blockStartTime{text-align: center;padding: 5px 5px 5px 5px}
+  .blockEndTime{text-align: center;padding: 5px 5px 5px 5px}
 
 </style>
